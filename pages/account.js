862469750062bd7ed24c33960554cb07f0f6e99a
robgmerrill <robgmerrill@gmail.com>
2022-12-04 15:29:43 -0800

    add starter code and routes

---
+++ pages/account.js
@@ -0,0 +1,7 @@
+export default function AccountPage() {
+  return (
+    <div>
+      <h1>The Account Page</h1>
+    </div>
+  );
+}
---
+++ pages/products.js
@@ -0,0 +1,7 @@
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>The Products Page</h1>
+    </div>
+  );
+}

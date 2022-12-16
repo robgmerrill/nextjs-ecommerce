b1059def9fb230b80b1ef48de8ac4e608884ce7a
robgmerrill <robgmerrill@gmail.com>
2022-12-15 18:07:29 -0800

    Add product add form and useForm custom hook

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bede80e6b758919d1d22"; }
+/******/ 		__webpack_require__.h = function() { return "8398b1b31fb68a544920"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/sell.js
+++ pages/sell.js
@@ -1,7 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
 export default function SellPage() {
   return (
     <div>
-      <h1>Sell Page</h1>
+      <CreateProduct />
     </div>
   );
 }

40dafe005b18ed19e92bc6abda513b4807305198
robgmerrill <robgmerrill@gmail.com>
2022-12-15 16:59:41 -0800

    Fix nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb10c03fd9e663b93bc8"; }
+/******/ 		__webpack_require__.h = function() { return "bede80e6b758919d1d22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

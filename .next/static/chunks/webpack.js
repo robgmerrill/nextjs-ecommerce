4ce67e5f16f303175670c66fb9023838772bd2f7
robgmerrill <robgmerrill@gmail.com>
2022-12-15 16:57:10 -0800

    fetch data and display it with Product component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "250ecb2cd15e7cc6773d"; }
+/******/ 		__webpack_require__.h = function() { return "cb10c03fd9e663b93bc8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
